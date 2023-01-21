ce3938d45be356c16a3e2c91d773cb2b4ea2c238
LeaMerrina <merrina.lea@hotmail.fr>
2023-01-21 23:50:04 +0100

    home responsive and animated

--- js/script.js
+++ js/script.js
@@ -1,45 +1,41 @@
-
 function SeeMore() {
-    
-    document.querySelector("header").classList.toggle("openNav");
-};
-
-document.addEventListener("DOMContentLoaded",function(){
-
-
-    document.getElementById("burger").onclick = function() {SeeMore()};
-
-var text = document.getElementById('title');
-var splitText = acAnimated.Plugins.SplitText(text, {words: 1, chars: 1, spacing: 40});
-
-
-
-const myFunction = () => {
-
-var tl = gsap.timeline({onComplete: myFunction});
-for (var i=0; i<=5; i++) {  
-
-	var char = splitText.chars[gsap.utils.random(0, 28,1)];
-    tl.fromTo(char,0.5,{color:'#ff0000'},{color:'#000000'},'-=0.4');
-
-};
-
-tl.resume();
-
-
+  document.querySelector("header").classList.toggle("openNav");
 }
 
-myFunction();
+function randomPositionCarre() {
+    let go = {onComplete: randomPositionCarre, ease: "power2.inOut"};
+    go.x= gsap.utils.random(-20, 450, 1)
+    go.y= gsap.utils.random(-20, 450, 1)
+    gsap.to('#carre', 2, go);
+}
 
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("burger").onclick = function () {
+    SeeMore();
+  };
 
+  const text = document.getElementById("title");
+  const splitText = acAnimated.Plugins.SplitText(text, {
+    words: 1,
+    chars: 1,
+    spacing: 40,
+  });
 
+  const colorText = () => {
+    var tl = gsap.timeline({ onComplete: colorText });
+    for (var i = 0; i <= 5; i++) {
+      var char = splitText.chars[gsap.utils.random(0, 28, 1)];
+      tl.fromTo(char, 0.5, { color: "#ff0000" }, { color: "#000000" }, "-=0.4");
+    }
 
+    tl.resume();
+  };
 
+  colorText();
+  randomPositionCarre();
 
-
-    setTimeout(() => {   
-    let tt =document.querySelector("header").offsetHeight+'px';
-    document.documentElement.style.setProperty('--navHeight', tt);
-}, "1000")
+  setTimeout(() => {
+    let tt = document.querySelector("header").offsetHeight + "px";
+    document.documentElement.style.setProperty("--navHeight", tt);
+  }, "1000");
 });
-
